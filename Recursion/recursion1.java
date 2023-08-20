f19887f4901c8997a43f68133dc8fb4b51ada294
Harshit0523 <harshupahade2004@gmail.com>
2023-08-20 14:24:22 +0530

    solved recursion problem

---
+++ Recursion/recursion1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class recursion1 {
+    public static void print(int n){
+        if(n==1){
+            System.out.println(n);
+            return;
+        }
+        print(n-1);
+        System.out.println(n);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        print(n);
+    }
+}
